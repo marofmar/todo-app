d62d13f310e7a7d91b13128e375ccae0797c5378
Yujin <yujin.mar.chung@gmail.com>
2025-04-22 18:11:40 +0900

    add: next-pwa 설치 및 config.js 업데이트

--- next.config.js
+++ next.config.js
@@ -1,3 +1,9 @@
+const withPWA = require("next-pwa")({
+  dest: "public",
+  register: true,
+  skipWaiting: true,
+});
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   webpack: (config, { isServer }) => {
@@ -17,4 +23,4 @@ const nextConfig = {
   },
 };
 
-module.exports = nextConfig;
+module.exports = withPWA(nextConfig);
